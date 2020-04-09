5688c969aa3551d0f2a933a66079178d88116629
Ivan Sokolov <ivan-p-sokolov@ya.ru>
2020-04-09 14:42:36 +0300

    Lower Elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule YuriTemplate.MixProject do
     [
       app: :yuri_template,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       dialyzer: [
