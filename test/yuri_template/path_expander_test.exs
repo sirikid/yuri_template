9b3296ea5b3bb02a9d71e9997337f69c23d4c668
Ivan Sokolov <ivan-p-sokolov@ya.ru>
2020-08-30 05:19:46 +0300

    Cover missing cases in PathExpander

--- test/yuri_template/path_expander_test.exs
+++ test/yuri_template/path_expander_test.exs
@@ -1,6 +1,7 @@
 defmodule YuriTemplate.PathExpanderTest do
   use ExpanderTest,
     cases: [
+      # Cases from RFC
       {"{/who}", "/fred"},
       {"{/who,who}", "/fred/fred"},
       {"{/half,who}", "/50%25/fred"},
@@ -14,6 +15,10 @@ defmodule YuriTemplate.PathExpanderTest do
       {"{/list*}", "/red/green/blue"},
       {"{/list*,path:4}", "/red/green/blue/%2Ffoo"},
       {"{/keys}", "/semi,%3B,dot,.,comma,%2C"},
-      {"{/keys*}", "/semi=%3B/dot=./comma=%2C"}
+      {"{/keys*}", "/semi=%3B/dot=./comma=%2C"},
+      # Additional cases for 100% coverage
+      {"foo{/undef*}", "foo"},
+      {"bar{/undef:10}", "bar"},
+      {"baz{/empty_keys}", "baz"}
     ]
 end
