8c73442778f7bc617bd1ae376d3cd8002b6c5269
Ivan Sokolov <ivan-p-sokolov@ya.ru>
2020-08-30 06:06:10 +0300

    Cover missing cases in ReservedExpander

--- test/yuri_template/reserved_expander_test.exs
+++ test/yuri_template/reserved_expander_test.exs
@@ -1,6 +1,7 @@
 defmodule YuriTemplate.ReservedExpanderTest do
   use ExpanderTest,
     cases: [
+      # Cases from RFC
       {"{+var}", "value"},
       {"{+hello}", "Hello%20World!"},
       {"{+half}", "50%25"},
@@ -17,6 +18,14 @@ defmodule YuriTemplate.ReservedExpanderTest do
       {"{+list}", "red,green,blue"},
       {"{+list*}", "red,green,blue"},
       {"{+keys}", "semi,;,dot,.,comma,,"},
-      {"{+keys*}", "semi=;,dot=.,comma=,"}
+      {"{+keys*}", "semi=;,dot=.,comma=,"},
+      # Additional cases for 100% coverage
+      {"foo{+undef*}", "foo"},
+      {"bar{+empty_keys*}", "bar"},
+      {"baz{+undef:6}", "baz"},
+      {"qux{+empty_keys}", "qux"},
+      {"{+var,undef}", "value"},
+      {"{+var,keys}", "value,semi,;,dot,.,comma,,"},
+      {"{+var,list}", "value,red,green,blue"}
     ]
 end
