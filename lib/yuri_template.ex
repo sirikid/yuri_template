1e22952ff3771a285da39975bb10d12fcf285142
Ivan Sokolov <ivan-p-sokolov@ya.ru>
2020-08-30 09:47:54 +0300

    Add docs for YuriTemplate and YuriTemplate.Expander

--- lib/yuri_template.ex
+++ lib/yuri_template.ex
@@ -1,9 +1,22 @@
 defmodule YuriTemplate do
+  @moduledoc """
+  Delegates and convenience wrappers.
+  """
+
   alias YuriTemplate.RFC6570
 
   @spec parse(String.t()) :: {:ok, RFC6570.t()} | {:error, term}
   defdelegate parse(str), to: RFC6570
 
+  @doc """
+  Expands template
+
+  ## Parameters
+
+  - template: `t:String.t/0` or `t:YuriTemplate.RFC6570.t/0`
+
+  - substitutes: `t:Access.t/0` mapping names to values
+  """
   @spec expand(String.t() | RFC6570.t(), Access.t()) :: {:ok, String.t()} | {:error, term}
   def expand(str, substitutes) when is_binary(str) do
     with {:ok, template} <- parse(str) do
@@ -15,6 +28,7 @@ defmodule YuriTemplate do
     {:ok, IO.iodata_to_binary(RFC6570.expand(template, substitutes))}
   end
 
+  @doc "Same as `expand/2`, but raises exception on error."
   @spec expand!(String.t() | RFC6570.t(), Access.t()) :: String.t()
   def expand!(str, substitutes) when is_binary(str) do
     case parse(str) do
--- lib/yuri_template/expander.ex
+++ lib/yuri_template/expander.ex
@@ -1,5 +1,25 @@
 defmodule YuriTemplate.Expander do
+  @moduledoc """
+  The most common interface for the template expander.
+  """
+
+  @typedoc """
+  Variable list specification.
+  """
   @type spec :: [atom | {:explode, atom} | {:prefix, atom, 1..10_000}]
 
-  @callback expand(iodata, Access.t(), spec) :: iodata
+  @doc """
+  Called to add an expansion of the next list of variables to the
+  accumulator.
+
+  ## Parameters
+
+  - accumulator: `t:iodata/0`, new fragments must be consed to it in
+    reverse order
+
+  - substitutes: `t:Access.t/0`, mapping names to values
+
+  - variables: list of names to get from `substitutes`
+  """
+  @callback expand(accumulator :: iodata, substitutes :: Access.t(), variables :: spec) :: iodata
 end
