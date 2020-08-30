2d8284886eb1b5a109d19ae0991714db84517e2c
Ivan Sokolov <ivan-p-sokolov@ya.ru>
2020-08-30 06:32:13 +0300

    Add tests for UnsafeExpander

--- test/yuri_template/unsafe_expander_test.exs
+++ test/yuri_template/unsafe_expander_test.exs
@@ -1,5 +1,22 @@
 defmodule YuriTemplate.UnsafeExpanderTest do
   use ExUnit.Case
 
-  doctest YuriTemplate.UnsafeExpander
+  alias YuriTemplate.UnsafeExpander, as: UE
+
+  describe "expand/3" do
+    test "no variables left" do
+      assert UE.expand("an accumulator", nil, []) == "an accumulator"
+    end
+
+    test "non existing variable" do
+      assert UE.expand("another accumulator", [], [:undef]) == "another accumulator"
+    end
+
+    test "existing variable" do
+      expected = ["https://example.org" | "accumulator"]
+      actual = UE.expand("accumulator", [var: "https://example.org"], [:var])
+
+      assert actual == expected
+    end
+  end
 end
