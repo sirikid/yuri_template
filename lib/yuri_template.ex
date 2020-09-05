a032d269fdd4ba560b9bca7f05a86f7a0c9826f0
Ivan Sokolov <ivan-p-sokolov@ya.ru>
2020-09-05 06:14:10 +0300

    Update YuriTemplate documentation
    and fix one little bug

--- lib/yuri_template.ex
+++ lib/yuri_template.ex
@@ -9,33 +9,38 @@ defmodule YuriTemplate do
   defdelegate parse(str), to: RFC6570
 
   @doc """
-  Expands template
+  Parses (if necessary) and expands the template.
 
   ## Parameters
 
-  - template: `t:String.t/0` or `t:YuriTemplate.RFC6570.t/0`
+  - `template`: the template for the expansion.
 
-  - substitutes: `t:Access.t/0` mapping names to values
+  - `substitutes`: mapping names to values.
   """
   @spec expand(String.t() | RFC6570.t(), Access.t()) :: {:ok, String.t()} | {:error, term}
+  def expand(template_or_string, substitutes)
+
   def expand(str, substitutes) when is_binary(str) do
     with {:ok, template} <- parse(str) do
-      expand(template, substitutes)
+      {:ok, expand_template(template, substitutes)}
     end
   end
 
   def expand(template, substitutes) do
-    {:ok, IO.iodata_to_binary(RFC6570.expand(template, substitutes))}
+    {:ok, expand_template(template, substitutes)}
   end
 
   @doc "Same as `expand/2`, but raises exception on error."
   @spec expand!(String.t() | RFC6570.t(), Access.t()) :: String.t()
+  def expand!(template_or_string, substitutes)
+
   def expand!(str, substitutes) when is_binary(str) do
     case parse(str) do
       {:ok, template} ->
-        template
-        |> RFC6570.expand(substitutes)
-        |> IO.iodata_to_binary()
+        expand_template(template, substitutes)
+
+      {:error, %{__exception__: true} = error} ->
+        raise error
 
       {:error, reason} ->
         raise RuntimeError, message: inspect(reason)
@@ -43,6 +48,13 @@ defmodule YuriTemplate do
   end
 
   def expand!(template, substitutes) do
-    RFC6570.expand(template, substitutes)
+    expand_template(template, substitutes)
+  end
+
+  @spec expand_template(RFC6570.t(), Access.t()) :: String.t()
+  defp expand_template(template, substitutes) do
+    template
+    |> RFC6570.expand(substitutes)
+    |> IO.iodata_to_binary()
   end
 end
