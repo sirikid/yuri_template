66846ed0f5a8407749408a4ebf526f9c27cb9366
Ivan Sokolov <ivan-p-sokolov@ya.ru>
2021-04-18 23:42:22 +0300

    Bump version to 1.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule YuriTemplate.MixProject do
   def project do
     [
       app: :yuri_template,
-      version: "1.0.0",
+      version: "1.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
